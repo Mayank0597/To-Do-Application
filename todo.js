f7664bdf67ccfb3ba94d7a83cb35f470def4e6cf
MayankAgrawal <mayankagrawal0597@gmail.com>
2024-06-25 21:17:24 +0530

    todo app

---
+++ todo.js
@@ -0,0 +1,21 @@
+let todoList = [];
+displayItems();
+
+
+function addTodo(){
+    let inputElement = document.querySelector('#todo-input');
+    let todoItem = inputElement.value;
+   
+    todoList.push(todoItem);
+    inputElement.value='';
+    displayItems();
+}
+
+function displayItems(){
+    let displayElement = document.querySelector('#todo-items');
+    displayElement.innerHTML='';
+    for(let i=0;i<todoList.length;i++){
+        displayElement.innerText  = displayElement.innerText + "\n" + todoList[i] ;
+    }
+    
+}
